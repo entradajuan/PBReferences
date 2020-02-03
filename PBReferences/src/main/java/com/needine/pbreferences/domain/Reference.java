2535f9fdf487bcdb959ae353336503c4af17f3bb
Juan Ignacio <juan.lopezm@softtek.com>
2020-02-03 20:31:31 +0100

    domain

---
+++ PBReferences/src/main/java/com/needine/pbreferences/domain/Reference.java
@@ -0,0 +1,36 @@
+package com.needine.pbreferences.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+@Entity
+public class Reference {
+	
+	@Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    
+    private final String name;
+    private final String country;
+
+    // Empty constructor for JSON/JPA
+    Reference() {
+        this("", "");
+    }
+
+
+}
